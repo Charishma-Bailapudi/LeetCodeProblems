d2f46977e45d937713e21aa93bc7417dde74e8bb
Charishma Bailapudi <98004429+Charishma-Bailapudi@users.noreply.github.com>
2023-03-31 16:58:26 +0530

    Time: 89 ms (99.05%), Space: 61.1 MB (99.62%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -14,13 +14,27 @@ public:
         // }
         // return ans;
         
-        int n=nums.size();
-        int ans=0;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++)
+        
+        
+        // int n=nums.size();
+        // int ans=0;
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<n;i++)
+        // {
+        //     if((nums[i]^nums[i+1])==0)
+        //         return nums[i];
+        // }
+        // return 0;
+        
+        
+        
+        for(int i=0;i<nums.size();i++)
         {
-            if((nums[i]^nums[i+1])==0)
-                return nums[i];
+            int id=abs(nums[i]);
+            if(nums[id]<0)
+                return id;
+            else
+                nums[id]=-nums[id];
         }
         return 0;
         
