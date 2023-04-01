e26c74e8fe6325c4a696681d73e7bbe30f51ed5a
Charishma Bailapudi <98004429+Charishma-Bailapudi@users.noreply.github.com>
2023-04-01 14:03:22 +0530

    Added solution - LeetHub

---
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+	public:
+	const int M=1e9+7;
+	int perfectSum(int arr[], int n, int sum)
+	{
+        // Your code goes here
+        vector<vector<int>>dp(n+1,vector<int>(sum+1));
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                if(i==0&&j==0)
+                    dp[i][j]=1;
+                else if(i==0)
+                    dp[i][j]=0;
+                
+                else if(arr[i-1]>j)
+                    dp[i][j]=dp[i-1][j];
+                else
+                {
+                    int take=dp[i-1][j-arr[i-1]];
+                    int nottake=dp[i-1][j];
+                    dp[i][j]=(take+nottake)%M;
+                }
+                
+            }
+        }
+        return dp[n][sum];
+        // if(sum==0&&n==0)
+        //     return 1;
+        // if(n==0)
+        //     return 0;
+        // if(arr[n-1]>sum)
+        //     return perfectSum(arr,n-1,sum);
+        // int take=perfectSum(arr,n-1,sum-arr[n-1]);
+        // int nottake=perfectSum(arr,n-1,sum);
+        // return (take+nottake);
+	}
+	  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum;
+
+        cin >> n >> sum;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.perfectSum(a, n, sum) << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
