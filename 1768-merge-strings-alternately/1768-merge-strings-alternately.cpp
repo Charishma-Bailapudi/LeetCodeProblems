583166beeaa62dbe620f3d45516511c55ac6eb1b
Charishma Bailapudi <98004429+Charishma-Bailapudi@users.noreply.github.com>
2023-04-18 22:26:24 +0530

    Time: 8 ms (10.49%), Space: 8.1 MB (8.57%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,20 +1,48 @@
 class Solution {
 public:
-    string mergeAlternately(string word1, string word2) {
-        int m = word1.size();
-        int n = word2.size();
-        string result = "";
-        int i = 0, j = 0;
-
-        while (i < m || j < n) {
-            if (i < m) {
-                result.push_back(word1[i++]);
-            }
-            if (j < n) {
-                result.push_back(word2[j++]);
-            }
+    string merging(string word1, string word2, string &newstring, int i, int j) {
+        if (i < word1.length() && j < word2.length()) 
+        {
+            newstring.push_back(word1[i]);
+            newstring.push_back(word2[j]);
+            return merging(word1, word2, newstring, i+1, j+1);
         }
-
-        return result;
+        if (i == word1.length())
+        {
+            newstring.append(word2.substr(j));
+        }
+        else if (j == word2.length())
+        {
+            newstring.append(word1.substr(i));
+        }
+        return newstring;
+    }
+public:
+    string mergeAlternately(string word1, string word2) {
+        string newstring = "";
+        int i = 0, j = 0;
+        return merging(word1, word2, newstring, i, j);
     }
 };
+
+
+// class Solution {
+// public:
+//     string mergeAlternately(string word1, string word2) {
+//         int m = word1.size();
+//         int n = word2.size();
+//         string result = "";
+//         int i = 0, j = 0;
+
+//         while (i < m || j < n) {
+//             if (i < m) {
+//                 result.push_back(word1[i++]);
+//             }
+//             if (j < n) {
+//                 result.push_back(word2[j++]);
+//             }
+//         }
+
+//         return result;
+//     }
+// };
