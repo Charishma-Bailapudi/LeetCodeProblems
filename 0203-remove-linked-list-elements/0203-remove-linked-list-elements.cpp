e0bd0182e2dde9f8a7a3dd70c75dfa5cf3368894
Charishma Bailapudi <98004429+Charishma-Bailapudi@users.noreply.github.com>
2023-05-24 20:39:15 +0530

    Time: 30 ms (20.03%), Space: 15 MB (75.49%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -8,24 +8,52 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val)
+//     {
+//        ListNode* dummy = new ListNode;
+//         dummy ->next = head;
+//         ListNode* curr = dummy;
+//         while(curr -> next)
+//         {
+//             if(curr -> next -> val == val)
+//             {
+//                 curr -> next = curr -> next -> next;
+//             } 
+//             else
+//             {
+//                 curr = curr -> next;
+//             }
+//         }
+//         return dummy -> next;
+//     }
+// };
 class Solution {
 public:
-    ListNode* removeElements(ListNode* head, int val)
-    {
-       ListNode* dummy = new ListNode;
-        dummy ->next = head;
-        ListNode* curr = dummy;
-        while(curr -> next)
-        {
-            if(curr -> next -> val == val)
-            {
-                curr -> next = curr -> next -> next;
-            } 
-            else
-            {
-                curr = curr -> next;
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode*prev= head;
+        ListNode*curr= head;
+
+        while(curr != NULL){
+        //when head node is the node to be removed from linked list
+            if(head->val == val){
+                head= head->next;
+                prev= head;
+                curr= head;
+            }
+        //when current node is the node to be deleted
+            else if(curr->val==val){
+                prev->next= curr->next;
+                curr= curr->next;
+            }
+        // when neither head or curr node is to be deleted
+        // then we simply update values of curr and prev pointer
+            else{
+                prev= curr;
+                curr= curr->next;
             }
         }
-        return dummy -> next;
+        return head;
     }
 };
