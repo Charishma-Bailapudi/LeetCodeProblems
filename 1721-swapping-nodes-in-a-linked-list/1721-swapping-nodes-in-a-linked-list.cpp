5167d67e32671736399da4d46647c6f0a4d2d536
Charishma Bailapudi <98004429+Charishma-Bailapudi@users.noreply.github.com>
2023-05-24 23:04:19 +0530

    Time: 719 ms (36.51%), Space: 180.1 MB (71.24%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -10,26 +10,46 @@
  */
 class Solution {
 public:
-    ListNode* swapNodes(ListNode* head, int k)
-    {
-        vector<int>v;
-        while(head!=NULL)
-        {
-            v.push_back(head->val);
-            head=head->next;
-        }
-        int n=v.size()-k;
-        int temp=v[k-1];
-        v[k-1]=v[n];
-        v[n]=temp;
-         ListNode* ans=new  ListNode(0);
-         ListNode* res=ans;
-        for(int i:v)
-        {
-            res->next=new  ListNode(i);
-            res=res->next;
-        }
-        return ans->next;
+//     ListNode* swapNodes(ListNode* head, int k)
+//     {
+//         vector<int>v;
+//         while(head!=NULL)
+//         {
+//             v.push_back(head->val);
+//             head=head->next;
+//         }
+//         int n=v.size()-k;
+//         int temp=v[k-1];
+//         v[k-1]=v[n];
+//         v[n]=temp;
+//          ListNode* ans=new  ListNode(0);
+//          ListNode* res=ans;
+//         for(int i:v)
+//         {
+//             res->next=new  ListNode(i);
+//             res=res->next;
+//         }
+//         return ans->next;
         
+//     }
+     ListNode* swapNodes(ListNode* head, int k) 
+     {
+        ListNode *left_ptr = head;
+        ListNode *right_ptr = head;
+        for (int i = 0; i < k-1; i++) 
+        {
+            right_ptr = right_ptr->next;
+        }
+        
+        ListNode *end_ptr = right_ptr;
+        while (right_ptr->next)
+        {
+            left_ptr = left_ptr->next;
+            right_ptr = right_ptr->next;
+        }
+         
+        swap(end_ptr->val, left_ptr->val);
+        
+        return head;
     }
 };
