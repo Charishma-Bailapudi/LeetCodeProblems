62d7a1544fb2c3d7220e24d1689a5d56a8e2a18e
Charishma Bailapudi <98004429+Charishma-Bailapudi@users.noreply.github.com>
2023-05-30 11:47:43 +0530

    Time: 262 ms (88.90%), Space: 116.5 MB (81.49%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -8,26 +8,71 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     int pairSum(ListNode* head)
+//     {
+//         vector<int>v;
+//         ListNode* temp=head;
+//         while(temp!=NULL)
+//         {
+//             v.push_back(temp->val);
+//             temp=temp->next;
+//         }
+//         int start=0,end=v.size()-1;
+//         int tot,sum=0;
+//         while(start<=end)
+//         {
+//             tot=v[start]+v[end];
+//             if(tot>sum)
+//                 sum=tot;
+//             start++;
+//             end--;
+//         }
+//         return sum;
+//     }
+// };
 class Solution {
 public:
+    ListNode* reverseList(ListNode* head)
+    {
+        if (head == NULL) {
+            return NULL;
+        }
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        while (curr->next != NULL) 
+        {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        curr->next = prev;
+        return curr;
+    }
+
     int pairSum(ListNode* head)
     {
-        vector<int>v;
-        ListNode* temp=head;
-        while(temp!=NULL)
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast->next)
         {
-            v.push_back(temp->val);
-            temp=temp->next;
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        int start=0,end=v.size()-1;
+        ListNode* temp=slow->next;
+        ListNode* reverse = reverseList(temp);
+        slow->next=NULL;
         int tot,sum=0;
-        while(start<=end)
+        while (head && reverse)
         {
-            tot=v[start]+v[end];
-            if(tot>sum)
-                sum=tot;
-            start++;
-            end--;
+            tot = head->val + reverse->val;
+            // cout<<"tot"<<tot<<endl;
+            if (tot >= sum)
+                sum = tot;
+            head = head->next;
+            reverse = reverse->next;
         }
         return sum;
     }
