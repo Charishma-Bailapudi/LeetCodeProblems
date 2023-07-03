fd41a3b17228daedd0581260cd7ae55f5842419b
Charishma Bailapudi <98004429+Charishma-Bailapudi@users.noreply.github.com>
2023-07-03 18:09:19 +0530

    Time: 60 ms (22.41%), Space: 45.1 MB (16.51%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void generate(vector<int>& nums,int n,int target,vector<int> subsets)
+    {
+        if(target == 0)
+        {
+            ans.push_back(subsets);
+            return;
+        }
+        if(n == 0)
+            return;
+       
+        if(nums[n-1] > target)
+            generate(nums,n-1,target,subsets);
+        else {
+            subsets.push_back(nums[n-1]);
+            generate(nums,n,(target-nums[n-1]),subsets);
+            subsets.pop_back();
+            generate(nums,n-1,target,subsets);
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> subsets;
+        int n = candidates.size();
+        generate(candidates,n,target,subsets);
+        return ans; 
+    }
+};
