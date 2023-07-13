73ffac0997f03ef110c8f34bffa93117d052c130
Charishma Bailapudi <98004429+Charishma-Bailapudi@users.noreply.github.com>
2023-07-13 22:01:55 +0530

    Time: 12 ms (28.10%), Space: 13.1 MB (51.07%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -53,16 +53,7 @@ public:
                 }
             }
         }
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                if(vis[i][j] != 2  && grid[i][j] == 1)
-                {
-                    return -1;
-                }
-            }
-        }
+        
         if(cnt != cntfresh)
             return -1;
         return time;
