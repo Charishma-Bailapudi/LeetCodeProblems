9f159d97e3afdd497aabb8027868b53650295109
Charishma Bailapudi <98004429+Charishma-Bailapudi@users.noreply.github.com>
2023-08-29 21:38:15 +0530

    Time: 21 ms (87.90%), Space: 10.6 MB (86.10%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+      int bestClosingTime(string cs)    {
+         int con=0,mx=0,res=0;
+            for(int i=0;i<cs.length();i++)
+            {
+                if(cs[i]=='Y')
+                   con++;
+                else
+                    con--;
+                if(con>mx)
+                {
+                    mx=con;
+                    res=i+1;
+                }
+            }
+        return res;
+    }
+};
