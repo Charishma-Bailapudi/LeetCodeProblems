d8294e06641b370ca6b9aaf86b0d84375a0db425
Charishma Bailapudi <98004429+Charishma-Bailapudi@users.noreply.github.com>
2023-12-12 20:43:51 +0530

    Time: 15 ms (26.48%), Space: 14.7 MB (6.85%) - LeetHub

---
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) 
+    {
+        map<int,int>mp;
+        for(int i=0;i<arr.size();i++)
+        {
+            mp[arr[i]]++;
+        }
+        int maxi=0;
+      
+        for(auto x:mp)
+        {
+            double occ = (static_cast<double>(x.second) / arr.size()) * 100.0; 
+            if(occ>25.0)
+            {
+                maxi=x.first;
+                break;
+            }
+        }
+        return maxi;
+    }
+};
